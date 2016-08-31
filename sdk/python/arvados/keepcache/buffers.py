6a32949be05c2a8227c60e93fc7ccef9b5fa1522
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-08-31 14:58:26 +0100

    Fixes threading issues in pseudo buffer.

--- sdk/python/arvados/keepcache/buffers.py
+++ sdk/python/arvados/keepcache/buffers.py
@@ -1,7 +1,7 @@
 import logging
 from abc import ABCMeta
 from abc import abstractmethod
-from threading import Lock, Condition, Event
+from threading import Lock, Condition, Event, RLock
 
 _logger = logging.getLogger(__name__)
 
@@ -47,21 +47,18 @@ class _BlockControl(object):
     """
     def __init__(self):
         self.counter = 0
-        self.condition = Condition()
-        self.entry_allowed = Event()
-        self.entry_allowed.set()
+        self.exit_condition = Condition()
+        self.entry_lock = RLock()
 
     def __enter__(self):
-        if not self.entry_allowed.is_set():
-            # Prevent entry into block until allowed again
-            self.entry_allowed.wait()
-        with self.condition:
+        with self.entry_lock:
             self.counter += 1
 
     def __exit__(self, *args, **kwargs):
-        with self.condition:
+        with self.exit_condition:
             self.counter -= 1
-            self.condition.notify_all()
+            assert self.counter >= 0
+            self.exit_condition.notify_all()
 
 
 class OpenTransactionBuffer(PseudoBuffer):
@@ -109,10 +106,13 @@ class OpenTransactionBuffer(PseudoBuffer):
         self._transaction = None
         self._buffer = None
         self._read_block_control = _BlockControl()
-        self._close_transaction_lock = Lock()
-        self._close_counter = 0
-        self._open_transaction_lock = Lock()
+        self._change_transaction_lock = Lock()
         self._stop_read_lock = Lock()
+        self._close_counter = 0
+        self._paused = False
+
+    def __iter__(self):
+        return OpenTransactionBuffer._BufferIterator(self)
 
     def __del__(self):
         if self._has_open_transaction():
@@ -121,81 +121,99 @@ class OpenTransactionBuffer(PseudoBuffer):
     def __getitem__(self, index):
         with self._read_block_control:
             self._open_transaction()
+            assert self._buffer is not None
             return self._buffer[index]
 
     def __len__(self):
         with self._read_block_control:
             self._open_transaction()
+            assert self._buffer is not None
             return len(self._buffer)
 
     def __eq__(self, other):
         with self._read_block_control:
             self._open_transaction()
+            assert self._buffer is not None
             if isinstance(other, type(self)):
                 return other._buffer == self._buffer
             # XXX: Loss of symmetric equality :(
             return other == self._buffer
 
-    def __iter__(self):
-        return OpenTransactionBuffer._BufferIterator(self)
-
     def __str__(self):
         with self._read_block_control:
             self._open_transaction()
+            assert self._buffer is not None
             return str(self._buffer)
 
     def pause(self):
         """
-        Pauses ability to read from the buffer.
+        Pauses ability to read from the buffer and waits for all current
+        readers to finish.
         """
         with self._stop_read_lock:
-            _logger.debug(
-                "Pausing access to buffer associated to `%s`" % self.locator)
-            self._read_block_control.entry_allowed.clear()
+            if not self._paused:
+                _logger.debug("Pausing access to buffer associated to `%s`"
+                              % self.locator)
+                self._read_block_control.entry_lock.acquire()
+
+                # Waits for readers to finish
+                while self._read_block_control.counter > 0:
+                    _logger.debug(
+                        "Waiting for %d reader(s) of buffer associated "
+                        "to `%s` to finish before transaction is closed"
+                        % (self._read_block_control.counter, self.locator))
+                    self._read_block_control.exit_condition.acquire()
+                    self._read_block_control.exit_condition.wait()
+                assert self._read_block_control.counter == 0
+                self._paused = True
+                _logger.debug("All readers finished - buffer for %s is paused"
+                              % self.locator)
 
     def resume(self):
         """
         Resumes ability to read from the buffer.
         """
         with self._stop_read_lock:
-            _logger.debug(
-                "Resuming access to buffer associated to `%s`" % self.locator)
-            self._read_block_control.entry_allowed.set()
+            if self._paused:
+                _logger.debug("Resuming access to buffer associated to `%s`"
+                              % self.locator)
+                self._paused = False
+                self._read_block_control.entry_lock.release()
 
     def close_transaction(self):
         """
         Closes the transaction through which the buffer data is accessed.
         """
         if self._has_open_transaction():
-            # Copy value of close counter
-            closed = self._close_counter
-
-            with self._stop_read_lock:
-                # Ensures transaction was not already closed whilst waiting for lock
-                if self._close_counter == closed:
-                    # Prevent addition reads from the buffer
-                    self._read_block_control.entry_allowed.clear()
-
-                    # Waits for all buffer reads to finish
-                    while True:
-                        self._read_block_control.condition.acquire()
-                        if self._read_block_control.counter == 0:
-                            break
-                        _logger.debug(
-                            "Waiting for %d reader(s) of buffer associated "
-                            "to `%s` to finish before transaction is closed"
-                            % (self._read_block_control.counter, self.locator))
-                        # Wait for another reader to complete
-                        self._read_block_control.condition.wait()
-
+            with self._change_transaction_lock:
+                if self._has_open_transaction():
+                    if not self._paused:
+                        self.pause()
                     _logger.info(
                         "Closing transaction for buffer associated to `%s`"
                         % self.locator)
                     self._transaction_closer(self._transaction)
                     self._transaction = None
-                    self._read_block_control.entry_allowed.set()
-                    self._read_block_control.condition.release()
-                    self._close_counter += 1
+                    self._buffer = None
+                    # self._read_block_control.entry_allowed.set()
+                    # self._read_block_control.condition.release()
+
+    def _open_transaction(self):
+        """
+        Opens the transaction required to read from the buffer.
+        """
+        if not self._has_open_transaction():
+            with self._change_transaction_lock:
+                # Ensures transaction not opened whilst waiting for lock
+                if not self._has_open_transaction():
+                    _logger.info("Opening transaction for buffer associated "
+                                 "to `%s`" % self.locator)
+                    assert self._transaction is None
+                    assert self._buffer is None
+                    self._transaction = self._transaction_opener()
+                    self._buffer = self._transaction.get(self.locator)
+                    if self._buffer is None:
+                        raise OpenTransactionBuffer._NO_LONGER_VALID_ERROR
 
     def _has_open_transaction(self):
         """
@@ -203,22 +221,4 @@ class OpenTransactionBuffer(PseudoBuffer):
         :return: whether the transaction is open
         :rtype: bool
         """
-        return self._transaction is not None
-
-    def _open_transaction(self):
-        """
-        Opens the transaction required to read from the buffer.
-        """
-        if not self._has_open_transaction():
-            with self._stop_read_lock:
-                # Ensures transaction not opened whilst waiting for lock
-                if not self._has_open_transaction():
-                    assert self._transaction is None
-                    _logger.info("Opening transaction for buffer associated "
-                                 "to `%s`" % self.locator)
-                    self._transaction = self._transaction_opener()
-                    self._buffer = self._transaction.get(self.locator)
-                    _logger.debug("Got content from LMDB for `%s`" % self.locator)
-                    if self._buffer is None:
-                        raise OpenTransactionBuffer._NO_LONGER_VALID_ERROR
-        assert self._buffer is not None
+        return self._transaction is not None and self._buffer is not None
