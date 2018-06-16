1878fd2c510139176f7c8ea0427179a39218b530
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-06-16 13:23:47 +0100

    call JSON.dump rather than .to_json

--- sdk/cwl/arvados_cwl/arvcontainer.py
+++ sdk/cwl/arvados_cwl/arvcontainer.py
@@ -317,7 +317,7 @@ class RunnerContainer(Runner):
             "mounts": {
                 "/var/lib/cwl/cwl.input.json": {
                     "kind": "text",
-                    "content": "%s" % self.job_order.to_json
+                    "content": "%s" % JSON.dump(self.job_order)
                 },
                 "stdout": {
                     "kind": "file",
@@ -351,7 +351,7 @@ class RunnerContainer(Runner):
             workflowpath = "/var/lib/cwl/workflow.json#main"
             container_req["mounts"]["/var/lib/cwl/workflow.json"] = {
                 "kind": "text",
-                "content": "%s" % packed.to_json
+                "content": "%s" % JSON.dump(packed)
             }
             if self.tool.tool.get("id", "").startswith("arvwf:"):
                 container_req["properties"]["template_uuid"] = self.tool.tool["id"][6:33]
