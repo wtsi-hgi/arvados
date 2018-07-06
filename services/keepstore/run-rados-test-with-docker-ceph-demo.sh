5b9a74df17c4cc1ff2bea6759c5d568c0c686076
Joshua C. Randall <jcrandall@alum.mit.edu>
2018-07-06 01:28:39 +0000

    fix rados test docker run script so that it waits for ceph to be ready

--- services/keepstore/run-rados-test-with-docker-ceph-demo.sh
+++ services/keepstore/run-rados-test-with-docker-ceph-demo.sh
@@ -8,18 +8,51 @@ user=client.keeptest
 pool=keeptest
 
 echo "Creating ceph/demo container"
-container=$(docker run -d --name=ceph-demo --net=host -e MON_IP=${ip} -e CEPH_PUBLIC_NETWORK=${network} ceph/demo)
+container=$(docker run -d --name=ceph-demo --net=host -e MON_IP=${ip} -e CEPH_PUBLIC_NETWORK=${network} -e CLUSTER=ceph ceph/demo)
 echo "Created ceph/demo container: ${container}"
 
-echo "Creating user ${user} with access to pool ${pool}"
-docker exec ${container} ceph auth add ${user} mon 'allow r' osd 'allow rwx pool=${pool}'
+echo "Creating ceph user ${user} with access to pool ${pool}"
+docker exec ${container} ceph auth add ${user} mon "allow r" osd "allow rwx pool=${pool}"
+
+echo "Creating ceph pool ${pool}"
+docker exec ${container} ceph osd pool create ${pool} 8 8 replicated
 
 echo "Getting key for user ${user}"
 key=$(docker exec ${container} ceph auth print-key ${user})
 
-echo "Running TestRados.* go tests using ceph pool ${pool} with user ${user} on mon-host ${ip}"
-go test -run 'TestRados.*' -test.rados-pool-volume ${pool} -rados-user ${user} -rados-mon-host ${ip} -rados-keyring-file <(echo -n "${key}") "$@" || export teststat=$?; true
+echo -n "Wait for ceph cluster to have nonzero raw_bytes..."
+raw_bytes=0
+while [[ "${raw_bytes}" -eq 0 ]]; do
+    sleep 1
+    raw_bytes=$(docker exec ${container} ceph -f json pg stat | perl -p -e 's/.*"raw_bytes":([0-9]+).*/$1/s')
+    echo -n "."
+done
+echo
+echo "Cluster has "${raw_bytes}" bytes"
+
+echo -n "Wait for ceph cluster to be healthy..."
+health=""
+while [[ "${health}" != "HEALTH_OK" ]]; do
+    sleep 1
+    health=$(docker exec ${container} ceph -s | awk '$1=="health" {print $2}')
+    echo -n "."
+done
+echo
+echo "${health}"
+
+keyringfile=$(mktemp)
+echo "Storing key in temporary keyringfile ${keyringfile}"
+cat << EOF > "${keyringfile}"
+[${user}]
+        key = ${key}
+EOF
+
+echo "Running TestRados.* go tests using ceph pool ${pool} on mon-host ${ip} with user ${user} and keyring-file ${keyringfile}"
+go test -run 'TestRados.*' -test.rados-pool-volume ${pool} -rados-mon-host ${ip} -rados-user ${user} -rados-keyring-file "${keyringfile}" "$@" || export teststat=$?; true
 echo "go test exit status ${teststat}"
+
+echo "Removing keyringfile ${keyringfile}"
+rm "${keyringfile}"
 
 echo "Killing and removing docker container ${container}"
 kc=$(docker kill ${container})
